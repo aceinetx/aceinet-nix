828d10ef862063b4f184b32b8df037aa4d6d7cbd
aceinetx <mouselin82@gmail.com>
2026-07-05 19:45:41 +0300

    something

--- bootloader.nix
+++ bootloader.nix
@@ -1,4 +1,4 @@
 {
   boot.loader.systemd-boot.configurationLimit = 10;
-  boot.extraModprobeConfig = ''options snd-intel-dspcfg dsp_driver=1'';
+  boot.extraModprobeConfig = "options snd-intel-dspcfg dsp_driver=1";
 }
--- packages.nix
+++ packages.nix
@@ -1,24 +1,52 @@
-{ lib, config, pkgs, ... }:
+{
+  lib,
+  config,
+  pkgs,
+  ...
+}:
 
 {
-  programs.hyprland.enable = true;
   programs.zsh.enable = true;
   programs.nix-ld.enable = true;
   programs.steam.enable = true;
   programs.java.enable = true;
+  programs.nix-ld.libraries = with pkgs; [
+    libX11
+    wayland
+    fontconfig
+    libxcursor
+    libxext
+    libxkbcommon
+    xinput
+  ];
+  programs.hyprland.enable = true;
 
   environment.systemPackages = with pkgs; [
-    javaPackages.compiler.openjdk17-bootstrap jdt-language-server
-    ghc haskellPackages.haskell-language-server
-    git github-cli forgejo-cli
-    man-pages man-pages-posix
-    discord telegram-desktop
-    clang clang-tools gcc
-    hyprpaper hyprlock
-    python3 pyrefly
-    readline rlwrap
-    neovim neovide
-    zig zls
+    javaPackages.compiler.openjdk17-bootstrap
+    jdt-language-server
+    ghc haskellPackages.haskell-language-server ormolu
+    git
+    github-cli
+    forgejo-cli
+    man-pages
+    man-pages-posix
+    discord
+    telegram-desktop
+    clang
+    clang-tools
+    gcc
+    hyprpaper
+    hyprlock
+    python3
+    pyrefly
+    readline
+    rlwrap
+    vim neovim
+    neovide
+    nil
+    nixfmt
+    zig
+    zls
     xmake
     rustup
     fastfetch
@@ -47,9 +75,20 @@
     grim
     slurp
     feh
-    bvi 
+    bvi
     file
     go
     vulkan-tools
+    setxkbmap
+    fasm
+    qrencode
+    zbar
+    llvm
+    steam-run
+    aseprite
+    pnpm
+    nodejs
+    godotPackages_4_7.godot
+    gdtoolkit_4
   ];
 }
