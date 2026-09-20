05594abc34f147a121db5d2f8d3a428f198cd43a
aceinetx <mouselin82@gmail.com>
2026-09-20 21:24:54 +0300

    more shit

--- modules/printer.nix
+++ modules/printer.nix
@@ -4,4 +4,14 @@
     enable = true;
     drivers = [ pkgs.pantum-driver ];
   };
+
+  services.avahi = {
+    enable = true;
+    nssmdns4 = true;
+    openFirewall = true;
+  };
+
+  users.users.aceinet.extraGroups = [ "lpadmin" ];
+
+  environment.etc.pantum-driver.source = "${pkgs.pantum-driver}/share";
 }
